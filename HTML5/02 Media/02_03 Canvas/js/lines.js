3ace195857875e08b116e5416d4348f086dd7077
Asia Skawinska <asiaska7@gmail.com>
2018-11-04 12:45:26 +0100

    html5wer1

---
+++ HTML5/02 Media/02_03 Canvas/js/lines.js
@@ -0,0 +1,10 @@
+	myCanvas_context.moveTo(25,190);
+	myCanvas_context.lineTo(40,180);
+	myCanvas_context.lineTo(90,150);
+	myCanvas_context.lineTo(100,165);
+	myCanvas_context.lineTo(130,90);
+	myCanvas_context.lineTo(150,100);
+	myCanvas_context.lineTo(275,20);
+    myCanvas_context.strokeStyle = "#000"; //jaki kolor ma mieć linia
+	myCanvas_context.stroke(); //komenda rysowania
+//move to i line to wskazują w którym miejscu będa przebiegac linie
